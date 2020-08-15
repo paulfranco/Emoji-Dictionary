ef2069dfdb132da5b0a83eb97e7e68ab8f914b8e
Paul Franco <mail@paulfran.co>
2020-08-14 23:45:16 -0700

    initial setup

---
+++ Emoji Dictionary/Emoji.swift
@@ -0,0 +1,22 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Paul Franco on 8/14/20.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
