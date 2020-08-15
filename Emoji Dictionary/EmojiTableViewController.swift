61221d536eddb8dd5053e6dcc0047a14f46a85f1
Paul Franco <mail@paulfran.co>
2020-08-15 00:22:24 -0700

    added table data

--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class EmojiTableViewController: UITableViewController {
+    
+    var emojis: [Emoji] = [
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: ":)", name: "Confused Face", description: "A confused, puzzled face", usage: "Unsure what to think"),
+        Emoji(symbol: ":)", name: "Heart Eyes", description: "A smiley face with hearts for eyes", usage: "Love of something attractive"),
+        Emoji(symbol: ":)", name: "Police Officer", description: "A police officere with a blue cap and a gold badge", usage: "Person of authority"),
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness"),
+        Emoji(symbol: ":)", name: "Grinning Face", description: "A typical smiley face", usage: "happiness")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cellLayoutMarginsFollowReadableWith = true
+        tableView.cellLayoutMarginsFollowReadableWidth = true
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -24,24 +37,28 @@ class EmojiTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return emojis.count
+        } else {
+            return 0
+        }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
 
-        // Configure the cell...
+        let emoji = emojis[indexPath.row]
+        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
+        cell.detailTextLabel?.text = emoji.description
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
