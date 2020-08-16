606089a9daebab66e1d0c0213fe4e60ac8ace233
Paul Franco <mail@paulfran.co>
2020-08-16 04:37:06 -0700

    created the EmojiTableViewCell

---
+++ Emoji Dictionary/EmojiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmojiTableViewCell.swift
+//  Emoji Dictionary
+//
+//  Created by Paul Franco on 8/16/20.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
